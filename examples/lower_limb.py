9ccf775604fad7c07b09c275d248ace43ef08699
Sheikh Mannan <45845727+sabdulm@users.noreply.github.com>
2025-05-09 14:00:35 -0600

    refactored code and update keypoint tracking for entire leg

--- examples/lower_limb.py
+++ examples/lower_limb.py
@@ -84,9 +84,9 @@ if __name__ == "__main__":
             DisplayFrame(color_output_frame),
             DisplayFrame(depth_output_frame),
             DisplayScene(displayScene, record=True, save_dir_prefix=save_dir_prefix),
-            SaveVideo(depth_output_frame, frame_rate=10, video_type="depth", delete_output=False, save_dir_prefix=save_dir_prefix),
-            SaveVideo(depth_output_frame_w_landmarks, frame_rate=10, video_type="depth", delete_output=False, save_dir_prefix=save_dir_prefix),
-            SaveVideo(color_output_frame, frame_rate=10, video_type="color", delete_output=False, save_dir_prefix=save_dir_prefix),
+            # SaveVideo(depth_output_frame, frame_rate=10, video_name="depth_only", delete_output=False, save_dir_prefix=save_dir_prefix),
+            # SaveVideo(depth_output_frame_w_landmarks, frame_rate=10, video_name="depth_w_landmarks", delete_output=False, save_dir_prefix=save_dir_prefix),
+            # SaveVideo(color_output_frame, frame_rate=10, video_type="color", delete_output=False, save_dir_prefix=save_dir_prefix),
             # ParadigmLog(gesture, body_tracking, depth, calibration, displayScene, csv=True, fileName="paradigm", output_dir=save_dir_prefix),
             #Log(transcriptions, stdout=True),
         ]
